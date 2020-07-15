2ad014ffbb2b8d4e60ec1d27ebc5a4935abb2cca
Alfred--Alan <l3210440292@gmail.com>
2020-07-15 09:57:59 +0800

    update layui

---
+++ src/components/work_order.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      1asas
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import index from '@/components/index'
+import work_order from '@/components/work_order'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'index',
+      component: index
+    },{
+      path: '/work_order',
+      name: 'work_order',
+      component: work_order
+    },
   ]
 })
